f88f3022f8f86ffe3d0c497b58fdc4d3b7e3169e
TomWis97 <github@tomwishaupt.nl>
2017-07-10 21:40:50 +0200

    Added rofi to i3 config

--- setup.sh
+++ setup.sh
@@ -6,6 +6,9 @@ sudo dnf install -y vim tmux thefuck powerline-fonts
 # Install powerline.
 sudo pip3 install --upgrade pip
 pip3 install --user powerline-status
+# Install i3-gaps
+sudo dnf copr enable yaroslav/i3desktop
+sudo dnf install i3-gaps compton rofi
 # Create config files.
 pwd=`pwd`
 rm ~/.bashrc
@@ -13,7 +16,9 @@ ln -s "$pwd/bashrc" ~/.bashrc
 ln -s "$pwd/vimrc" ~/.vimrc
 ln -s "$pwd/tmux.conf" ~/.tmux.conf
 ln -s "$pwd/i3config" ~/.config/i3/config
-ln -s "$pwd/compton.conf ~/.config/compton.conf
+ln -s "$pwd/compton.conf" ~/.config/compton.conf
+mkdir ~/.config/rofi
+ln -s "$pwd/rofi.conf" ~/.config/rofi/config
 # Set dconf settings.
 cat dconfsettings | dconf load /
 # Set dark theme.
