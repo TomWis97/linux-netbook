69a78ca1382ee02fbc5b609121f6a2fa9da32dc2
TomWis97 <github@tomwishaupt.nl>
2017-08-12 16:07:32 +0200

    Fix cower makepkg

--- bash_install/pacaur.sh
+++ bash_install/pacaur.sh
@@ -8,7 +8,7 @@ curl -O https://aur.archlinux.org/cgit/aur.git/snapshot/pacaur.tar.gz
 tar xvf cower.tar.gz
 cd cower
 gpg --recv-keys --keyserver hkp://pgp.mit.edu 1EB2638FF56C0C53
-makepkg
+makepkg -s
 makepkg -i
 cd ..
 tar xvf pacaur.tar.gz
