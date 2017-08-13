0b6fcc554779acc2e58dc0e519a188d32a6ed95a
TomWis97 <github@tomwishaupt.nl>
2017-08-13 10:52:23 +0200

    Added fonts

--- bash_install/fonts.sh
+++ bash_install/fonts.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -ex
 echo ---------- Installing fonts
+pacaur -S --noedit --noconfirm ttf-vista-fonts adobe-source-sans-pro-fonts ttf-ms-fonts ttf-liberation 
 cd /tmp
 mkdir ~/.fonts
 curl -OL https://github.com/ryanoasis/nerd-fonts/releases/download/v1.0.0/RobotoMono.zip
