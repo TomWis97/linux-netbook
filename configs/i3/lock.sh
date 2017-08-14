5e79ad0fde1354687b30e3fc0fcb3e7f07691ed9
TomWis97 <github@tomwishaupt.nl>
2017-08-14 15:06:05 +0000

    Added locking notification to Polybar

--- configs/i3/lock.sh
+++ configs/i3/lock.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
+polybar-msg hook notification 3
 resolution=`xrandr | fgrep '*' | cut -d ' ' -f 4`
 #echo "Resolution: $resolution"
 convert ~/Wallpapers/lockscreen.png -resize "$resolution^" -gravity center -extent $resolution /tmp/lockscreen.png
+polybar-msg hook notification 1
 i3lock -nef -i /tmp/lockscreen.png
 rm /tmp/lockscreen.png
