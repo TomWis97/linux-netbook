a782648267a663e928ba89b19f0590967acaa4cb
TomWis97 <github@tomwishaupt.nl>
2017-08-15 16:18:40 +0200

    Add caching of lockscreen background

--- configs/i3/lock.sh
+++ configs/i3/lock.sh
@@ -1,8 +1,23 @@
 #!/usr/bin/env bash
+create_back () {
+    convert ~/Wallpapers/lockscreen.png -resize "$resolution^" -gravity center -extent $resolution /tmp/lockscreen.png
+}
 polybar-msg hook notification 3
 resolution=`xrandr | fgrep '*' | cut -d ' ' -f 4`
+if [ -f /tmp/lockscreen.png ]; then
+    #echo "File exists"
+    imgres=`identify /tmp/lockscreen.png | cut -d ' ' -f 3`
+    if [ "$resolution" != "$imgres" ]; then
+        #echo "Different resolution!"
+        create_back
+    else
+        echo "Using cached lockscreen background"
+    fi
+else
+    #echo "File doesn't exist"
+    create_back
+fi
 #echo "Resolution: $resolution"
-convert ~/Wallpapers/lockscreen.png -resize "$resolution^" -gravity center -extent $resolution /tmp/lockscreen.png
 polybar-msg hook notification 1
 i3lock -nef -i /tmp/lockscreen.png
-rm /tmp/lockscreen.png
+#rm /tmp/lockscreen.png
